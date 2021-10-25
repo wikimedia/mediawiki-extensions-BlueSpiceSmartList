c10b6e6920cbd23d4ba1495002d53da874e35009
pwirth <wirth@hallowelt.biz>
2021-10-25 13:52:32 +0200

    Personal mega menu integration in BlueSpiceDiscovery skin
    Unfortunately the curretn widgets are hard coded to be
    BlueSpice\Calumma\IPanel and therefor we need a new registry
    
    ERM:25779
    [4.1]
    
    Change-Id: I8c946c04b788b622a5d4f352690aa4262677a67c

---
+++ src/UserSidebar/Widget/YourEdits.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace BlueSpice\SmartList\UserSidebar\Widget;
+
+use BlueSpice\UserSidebar\Widget;
+use Message;
+
+class YourEdits extends Widget {
+	public const PARAM_COUNT = 'count';
+
+	/**
+	 *
+	 * @return bool
+	 */
+	public function shouldRender(): bool {
+		return !$this->context->getUser()->isAnon();
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getHeaderMessage(): Message {
+		return $this->context->msg( 'bs-smartlist-lastedits' );
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getLinks(): array {
+		if ( $this->context->getUser()->isAnon() ) {
+			return [];
+		}
+		$count = 5;
+		if ( isset( $this->params[static::PARAM_COUNT] ) ) {
+			$count = (int)$this->params[static::PARAM_COUNT];
+		}
+
+		$edits = \SmartList::getYourEditsTitles( $this->context->getUser(), $count );
+
+		$links = [];
+		foreach ( $edits as $edit ) {
+			$link = [
+				'href' => $edit['title']->getFullURL(),
+				'text' => $edit['displayText'],
+				'title' => $edit['displayText'],
+				'classes' => ' bs-usersidebar-internal '
+			];
+			$links[] = $link;
+		}
+
+		return $links;
+	}
+
+}
