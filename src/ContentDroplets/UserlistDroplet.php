fcc4001ec8afe50239d39a0b4d23fd69b1b86233
hslater <hamish.slater@hallowelt.com>
2025-01-22 11:09:46 +0100

    Update Message namespace
    ERM38444
    
    [REL1_43, master]
    
    Change-Id: Iaa02ddffb31ebda919535835b94fffb7c754ac6a

--- src/ContentDroplets/UserlistDroplet.php
+++ src/ContentDroplets/UserlistDroplet.php
@@ -3,7 +3,7 @@
 namespace BlueSpice\SmartList\ContentDroplets;
 
 use MediaWiki\Extension\ContentDroplets\Droplet\TagDroplet;
-use Message;
+use MediaWiki\Message\Message;
 
 class UserlistDroplet extends TagDroplet {
 
