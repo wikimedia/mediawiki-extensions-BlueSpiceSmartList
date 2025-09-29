8d2814dabf86d6b71a1c02a254c05ca2b978a25b
d.savuljesku <d.savuljesku@gmail.com>
2025-09-29 12:40:13 +0200

    Cache namespace permission checking
    [5.1.x]
    
    ERM53578
    
    Change-Id: I23be2db002ce12f298e49055320a204d35f597cc

--- src/Mode/SmartListBaseMode.php
+++ src/Mode/SmartListBaseMode.php
@@ -2,10 +2,17 @@
 
 namespace BlueSpice\SmartList\Mode;
 
+use MediaWiki\Linker\LinkTarget;
+use MediaWiki\Permissions\PermissionManager;
+use MediaWiki\User\User;
+
 abstract class SmartListBaseMode extends BaseMode {
 
 	public const ATTR_CAT = 'cat';
 	public const ATTR_NS = 'ns';
+
+	/** @var array */
+	private array $restrictedNamespaces = [];
 
 	/**
 	 *
@@ -22,6 +29,23 @@ abstract class SmartListBaseMode extends BaseMode {
 			'required' => false,
 		];
 		return $params;
+	}
+
+	/**
+	 * @param LinkTarget $page
+	 * @param User $user
+	 * @param PermissionManager $permissionManager
+	 * @return bool
+	 */
+	protected function userCanRead( LinkTarget $page, User $user, PermissionManager $permissionManager ): bool {
+		if ( in_array( $page->getNamespace(), $this->restrictedNamespaces ) ) {
+			return false;
+		}
+		if ( !$permissionManager->quickUserCan( 'read', $user, $page ) ) {
+			$this->restrictedNamespaces[] = $page->getNamespace();
+			return false;
+		}
+		return true;
 	}
 
 }
