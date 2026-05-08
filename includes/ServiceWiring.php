e1a75cce7f35fad3b6c475204b05a3df38aa580c
d.savuljesku <d.savuljesku@gmail.com>
2026-05-08 16:23:50 +0200

    Improve user edits query
    [5.1.x]
    
    ERM47492
    
    Change-Id: Ifbd4c90b2cc933f75ba60476ecd061d3e2adad0d

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -1,6 +1,7 @@
 <?php
 
 use BlueSpice\SmartList\BlueSpiceSmartListModeFactory;
+use BlueSpice\SmartList\UserEditProvider;
 use MediaWiki\MediaWikiServices;
 
 return [
@@ -9,5 +10,14 @@ return [
 		return new BlueSpiceSmartListModeFactory(
 			$objectFactory
 		);
-	}
+	},
+	'BlueSpiceSmartList.UserEditProvider' => static function ( MediaWikiServices $services ) {
+		return new UserEditProvider(
+			$services->getObjectCacheFactory(),
+			$services->getActorNormalization(),
+			$services->getDBLoadBalancer(),
+			$services->getTitleFactory(),
+			$services->getService( 'MWStakeCommonUtilsFactory' )
+		);
+	},
 ];
